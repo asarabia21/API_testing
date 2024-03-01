beeea2d547630d6fa04d49e7243064ff2e7a936e
tjbolen <159741478+tjbolen@users.noreply.github.com>
2024-03-01 09:37:57 -0500

    TJ Changes 01 MAR
    Error corrected and somewhat useful file now exists in testscript2

--- testscript2_no_praw.py
+++ testscript2_no_praw.py
@@ -4,8 +4,8 @@ import csv
 from pprint import pprint
 
 # Reddit API Credentials
-client_id = "Ez0xGK31vry7_EdElEp2dQ"
-secret = "dQE-dZcJFQMg8RDNvI6jNyJuMu62KA"
+client_id = "Ct-3GfVL4b5POH-Q3fdiSQ"
+secret = "X-JF_RAbNYo-S76wYqW0iwbumwzdgQ"
 
 # Reddit user credentials
 username = "estabanmagnifico"
@@ -14,7 +14,7 @@ password = "Monkey5000"
 # Reddit API access token
 auth = requests.auth.HTTPBasicAuth(client_id, secret)
 data = {"grant_type": "password", "username": username, "password": password}
-headers = {"User-Agent": "testscript by u/estabanmagnifico"}
+headers = {"User-Agent": "testscript2_no_praw by u/estabanmagnifico"}
 r = requests.post("https://www.reddit.com/api/v1/access_token", auth=auth, data=data, headers=headers)
 reddit_token = r.json()["access_token"]
 
@@ -22,11 +22,22 @@ reddit_token = r.json()["access_token"]
 headers = {**headers, **{"Authorization": f"bearer {reddit_token}"}}
 
 # Keyword Search
-q_list = ["DoD", "Army", "Military"]
-
+q_list = ["OSINT"]
 
 for q in q_list:
 	permalink_list = []
-	r = requests.get(f"https://oauth.reddit.com/r/all/search.jason?q={q}", headers=headers)
+	r = requests.get(f"https://oauth.reddit.com/r/all/search.json?q={q}", headers=headers)
 	r_json = r.json()
-	pprint(r_json)
+	#pprint(r_json)
+	for post in r_json["data"]["children"]:
+		permalink = f"https://www.reddit.com{post['data']['permalink']}"
+		print(permalink)
+		permalink_list.append([permalink])
+	with open(f"{q.replace(' ', '-')}.csv", "w", newline='') as file:
+		writer = csv.writer(file)
+		writer.writerows(permalink_list)
+
+	
+
+
+
